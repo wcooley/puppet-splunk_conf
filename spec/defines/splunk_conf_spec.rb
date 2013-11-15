aa676be31899bb52fcfe22c327e8d67a1f029047
Wil Cooley <wcooley@nakedape.cc>
2013-11-14 17:42:45 -0800

    Add a little basic test

--- spec/defines/splunk_conf_spec.rb
+++ spec/defines/splunk_conf_spec.rb
@@ -1,4 +1,11 @@
 require 'spec_helper'
 
 describe 'splunk_conf' do
+  context 'generic system' do
+    let(:title) { 'monitor:///foo/bar/baz.log' }
+    let(:params) { {
+        :config_file => '/tmp/test-splunk-config.conf',
+    } }
+    it { should contain_augeas('splunk_conf-monitor:///foo/bar/baz.log') }
+  end
 end
