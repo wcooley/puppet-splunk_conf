7b6c7da4aeb9bd9513f11ef0e9067d4a7747f83d
Wil Cooley <wcooley@nakedape.cc>
2013-11-25 17:01:11 -0800

    Refactor specs to make better spec output
    Conflicts:
    	spec/defines/splunk_conf_spec.rb

--- spec/defines/splunk_conf_spec.rb
+++ spec/defines/splunk_conf_spec.rb
@@ -1,47 +1,50 @@
 require 'spec_helper'
 
 describe 'splunk_conf' do
+  stanza = 'monitor:///foo/bar/baz.log'
 
-  context 'basic test' do
-    stanza = 'monitor:///foo/bar/baz.log'
-    let(:title) { stanza }
-    let(:params) do {
-        :config_file => 'test-splunk-config.conf',
-        :set => {
-          'index' => 'index_foo',
-          'sourcetype' => 'sourcetype_bar',
-        },
-        :rm => [ 'rmattr1', 'rmattr2' ],
-      }
+  context 'puppet catalog' do
+    context 'sets and rms' do
+      let(:title) { stanza }
+      let(:params) do {
+          :config_file => 'test-splunk-config.conf',
+          :set => {
+            'index' => 'index_foo',
+            'sourcetype' => 'sourcetype_bar',
+          },
+          :rm => [ 'rmattr1', 'rmattr2' ],
+        }
+      end
+
+      it 'has an augeas type with sets and rms' do
+        should contain_augeas("splunk_conf-#{stanza}").with({
+          :changes => [
+            "defnode target target[. = '#{stanza}'] #{stanza}",
+            "set $target/index index_foo",
+            "set $target/sourcetype sourcetype_bar",
+            "rm $target/rmattr1",
+            "rm $target/rmattr2",
+          ],
+          :incl => 'test-splunk-config.conf',
+        })
+      end
     end
 
-    it { should contain_augeas("splunk_conf-#{stanza}").with({
-        :changes => [
-          "defnode target target[. = '#{stanza}'] #{stanza}",
-          "set $target/index index_foo",
-          "set $target/sourcetype sourcetype_bar",
-          "rm $target/rmattr1",
-          "rm $target/rmattr2",
-        ],
-        :incl => 'test-splunk-config.conf',
-      })
-    }
-  end
+    context 'ensure => absent' do
+      let(:title) { stanza }
+      let(:params) do {
+          :config_file => 'test-splunk-config.conf',
+          :ensure => 'absent',
+        }
+      end
 
-  context 'remove stanza' do
-    stanza = 'monitor:///foo/bar/baz.log2'
-    let(:title) { stanza }
-    let(:params) do {
-        :config_file => 'test-splunk-config.conf',
-        :ensure => 'absent',
-      }
+      it 'has an augeas type that removes target' do
+        should contain_augeas("splunk_conf-#{stanza}").with({
+          :changes => "rm target[. = '#{stanza}']",
+          :incl => 'test-splunk-config.conf',
+        })
+      end
     end
-
-    it { should contain_augeas("splunk_conf-#{stanza}").with({
-        :changes => "rm target[. = '#{stanza}']",
-        :incl => 'test-splunk-config.conf',
-      })
-    }
   end
 end
 
