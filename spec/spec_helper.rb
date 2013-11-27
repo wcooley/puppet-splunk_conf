378061a1ddeefd87c22707cf8b1accee96642528
Wil Cooley <wcooley@nakedape.cc>
2013-11-26 20:31:19 -0800

    Set AUGEAS_LENS_LIB env

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,10 +1,11 @@
 require 'rspec-puppet'
 require 'puppetlabs_spec_helper/module_spec_helper'
-
 require 'rspec-puppet-augeas'
+
 RSpec.configure do |c|
     c.augeas_fixtures = File.join(File.dirname(File.expand_path(__FILE__)),
                                   'fixtures', 'augeas')
     c.augeas_lensdir = File.join(File.dirname(File.expand_path(__FILE__)),
                                   '..', 'lib/augeas/lenses')
+    ENV['AUGEAS_LENS_LIB'] = c.augeas_lensdir
 end
